d38c05e841722fe0e702fac931514287024a2af9
Nur Ihsan <ihsan@fas.id>
2022-09-24 18:45:57 +0800

    update baru

--- lib/presentation/pages/Izin.dart
+++ lib/presentation/pages/Izin.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/presentation/pages/Tmbh_Izin.dart';
+import 'package:intl/intl.dart';
 import '../resources/warna.dart';
 
 class Izin extends StatefulWidget {
@@ -10,8 +13,28 @@ class Izin extends StatefulWidget {
 }
 
 class _IzinState extends State<Izin> {
+  DateTime selectedPeriod = DateTime.now();
+  bool show = false;
+
+  Future<DateTime> _selectPeriod(BuildContext context) async {
+    final selected = await showDatePicker(
+        context: context,
+        initialDate: selectedPeriod,
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2025));
+    if (selected != null && selected != selectedPeriod) {
+      setState(() {
+        selectedPeriod = selected;
+      });
+    }
+    return selectedPeriod;
+  }
+
   @override
   Widget build(BuildContext context) {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    final user = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Warna.hijau2,
@@ -42,245 +65,189 @@ class _IzinState extends State<Izin> {
       ),
       body: Container(
         margin: EdgeInsets.only(top: 20),
-        padding: EdgeInsets.symmetric(horizontal: 20),
+        // padding: EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Title(
-                    child: Text(
-                      "Agustus 2022",
-                      style: TextStyle(
-                        color: Warna.hijau2,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    color: Warna.hijau2),
-                IconButton(
-                    icon: Icon(Icons.keyboard_arrow_down),
-                    color: Warna.hijau2,
-                    onPressed: () {}),
-              ],
-            ),
             Container(
-              alignment: Alignment.centerLeft,
-              margin: EdgeInsets.symmetric(vertical: 20),
               padding: EdgeInsets.symmetric(horizontal: 20),
-              height: 160,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(25), //border corner radius
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5), //color of shadow
-                    spreadRadius: 1, //spread radius
-                    blurRadius: 7, // blur radius
-                    offset: Offset(0, 2),
-                  ),
-                ],
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Row(
-                    children: [
-                      Text(
-                        "Pengajuan Lembur",
+                  Title(
+                      child: Text(
+                        DateFormat('MMMM yyyy').format(selectedPeriod),
                         style: TextStyle(
-                            backgroundColor: Warna.hijau2,
-                            fontSize: 15,
-                            color: Warna.htam),
-                      ),
-                      SizedBox(
-                        width: 30,
-                      ),
-                      TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          "Belum Disetujui",
-                          style: TextStyle(
-                            backgroundColor: Warna.mrah,
-                            fontSize: 15,
-                            color: Warna.htam,
-                          ),
+                          color: Warna.hijau2,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        "Minggu 07 Agustus 2022",
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Warna.htam,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.date_range,
-                        color: Warna.htam,
-                        size: 20.0,
-                      ),
-                      Text(
-                        "07 Agustus 2022 - 09 Agustus 2022",
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Warna.abuabu,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 7,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        "Izin Cuti",
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Warna.htam,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 30,
-                      ),
-                      Text(
-                        "Pulang Kampung",
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Warna.abuabu,
-                        ),
-                      ),
-                    ],
-                  )
+                      color: Warna.hijau2),
+                  IconButton(
+                      icon: Icon(Icons.keyboard_arrow_down),
+                      color: Warna.hijau2,
+                      onPressed: () {
+                        _selectPeriod(context);
+                        show = true;
+                      }),
                 ],
               ),
             ),
-            Container(
-              alignment: Alignment.centerLeft,
-              margin: EdgeInsets.symmetric(vertical: 20),
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              height: 160,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(25), //border corner radius
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5), //color of shadow
-                    spreadRadius: 1, //spread radius
-                    blurRadius: 7, // blur radius
-                    offset: Offset(0, 2),
-                  ),
-                ],
+            Flexible(
+                child: Container(
+              padding: EdgeInsets.only(bottom: 25),
+              child: StreamBuilder<QuerySnapshot>(
+                stream: firestore
+                    .collection("izin")
+                    .where("email", isEqualTo: user!.email)
+                    .where("month",
+                        isEqualTo: DateFormat("MMMM").format(selectedPeriod))
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  return !snapshot.hasData
+                      ? Center(
+                          child: CircularProgressIndicator(),
+                        )
+                      : ListView.builder(
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            DocumentSnapshot data = snapshot.data!.docs[index];
+                            return ItemCard(
+                                jenis: data['jenis'],
+                                ket: data['keterangan'],
+                                approve: data['status'],
+                                created_at: DateFormat('EEEE dd MMMM yyyy')
+                                    .format(data['created_at'].toDate())
+                                    .toString(),
+                                mulai: DateFormat('dd MMMM yyyy')
+                                    .format(data['tanggal_mulai'].toDate())
+                                    .toString(),
+                                selesai: DateFormat('dd MMMM yyyy')
+                                    .format(data['tanggal_selesai'].toDate())
+                                    .toString());
+                          },
+                        );
+                },
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        "Pengajuan Lembur",
-                        style: TextStyle(
-                            backgroundColor: Warna.hijau2,
-                            fontSize: 15,
-                            color: Warna.htam),
-                      ),
-                      SizedBox(
-                        width: 30,
-                      ),
-                      TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          "Disetujui",
-                          style: TextStyle(
-                            backgroundColor: Warna.hjau,
-                            fontSize: 15,
-                            color: Warna.htam,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        "Minggu 07 Agustus 2022",
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Warna.htam,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.date_range,
-                        color: Warna.htam,
-                        size: 20.0,
-                      ),
-                      Text(
-                        "07 Agustus 2022 - 09 Agustus 2022",
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Warna.abuabu,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 7,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        "Izin Cuti",
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Warna.htam,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 30,
-                      ),
-                      Text(
-                        "Pulang Kampung",
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Warna.abuabu,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
+            )),
           ],
         ),
+      ),
+    );
+  }
+
+  Container ItemCard(
+      {String? jenis,
+      String? ket,
+      String? approve,
+      String? created_at,
+      String? mulai,
+      String? selesai}) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      height: 160,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25), //border corner radius
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5), //color of shadow
+            spreadRadius: 1, //spread radius
+            blurRadius: 7, // blur radius
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Text(
+                "Pengajuan Lembur",
+                style: TextStyle(
+                    backgroundColor: Warna.hijau2,
+                    fontSize: 15,
+                    color: Warna.htam),
+              ),
+              SizedBox(
+                width: 30,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  approve == "1" ? "Disetujui" : "Belum Disetujui",
+                  style: TextStyle(
+                    backgroundColor: approve == "1" ? Warna.hjau : Warna.mrah,
+                    fontSize: 15,
+                    color: Warna.htam,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Text(
+                created_at ?? "",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Warna.htam,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.date_range,
+                color: Warna.htam,
+                size: 20.0,
+              ),
+              Text(
+                '${mulai} - ${selesai}',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Warna.abuabu,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 7,
+          ),
+          Row(
+            children: [
+              Text(
+                jenis ?? "",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Warna.htam,
+                ),
+              ),
+              SizedBox(
+                width: 30,
+              ),
+              Text(
+                ket ?? "",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Warna.abuabu,
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,14 @@ import cloud_firestore
 import firebase_auth
 import firebase_core
 import firebase_storage
+import platform_device_id
+import platform_device_id_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  PlatformDeviceIdMacosPlugin.register(with: registry.registrar(forPlugin: "PlatformDeviceIdMacosPlugin"))
+  PlatformDeviceIdMacosPlugin.register(with: registry.registrar(forPlugin: "PlatformDeviceIdMacosPlugin"))
 }
